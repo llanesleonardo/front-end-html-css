9f1a44ce38427e3ff6832071ee80d7296cbe91bb
llanesleonardo <llanesleonardo@gmail.com>
2022-10-13 19:03:17 -0700

    se fusionón la el menu mobile a menu desktop y se utilizó media queries para poder lograr el efecto de solamente utilizar el menu correspondiente en los disponitivos correspondientes

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 const navemail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const botonHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navemail.addEventListener("click", toggleSubMenu);
+botonHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleSubMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
