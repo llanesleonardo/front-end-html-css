48756983f29b4905092812785ec3e3534a07b57b
llanesleonardo <llanesleonardo@gmail.com>
2022-10-13 19:41:37 -0700

    se fusionó el aside que contiene el shopping cart al menu de escritorio y al menu mobile

--- scripts.js
+++ scripts.js
@@ -3,13 +3,48 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const botonHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const menuCarrito = document.querySelector(".navbar-shopping-cart");
+const menuAsideCart = document.querySelector(".my-cart");
+
 navemail.addEventListener("click", toggleSubMenu);
 botonHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarrito.addEventListener("click", toggleAsideCart);
 
 function toggleSubMenu() {
+  const isAsideMenuClosed = menuAsideCart.classList.contains("inactive");
+
+  if (!isAsideMenuClosed) {
+    //si el mobileMenu esta abierto, hay que cerrarlo
+    menuAsideCart.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isAsideMenuClosed = menuAsideCart.classList.contains("inactive");
+
+  if (!isAsideMenuClosed) {
+    //si el mobileMenu esta abierto, hay que cerrarlo
+    menuAsideCart.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleAsideCart() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isSubMenuClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    //si el mobileMenu esta abierto, hay que cerrarlo
+    mobileMenu.classList.add("inactive");
+  }
+
+  if (!isSubMenuClosed) {
+    //si el mobileMenu esta abierto, hay que cerrarlo
+    desktopMenu.classList.add("inactive");
+  }
+
+  menuAsideCart.classList.toggle("inactive");
 }
